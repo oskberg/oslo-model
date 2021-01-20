c9e2a69f9699bf4b71c3426e03d7e2818f3d8c89
oskhog <oskar.hogberg@gmail.com>
2021-01-20 13:22:49 +0100

    Correctly working oslo model implementation

--- model.py
+++ model.py
@@ -25,29 +25,28 @@ def propagate_avalanche(i):
         z_i_th[i] = int(np.random.random() > p) + 1
     return i
 # %%
-L = 5
+L = 32
 
 z_i_th = np.random.randint(1,3,L)
 z_i = np.zeros(L)
 h_i = np.zeros(L)
 
-p = 0.5
+p = 1/2
 
-iterations = 30
+h0 = []
+
+iterations = 10000
 for gen in range(iterations):
     site = 0
     z_i[0] += 1
     h_i[0] += 1
-    print(gen, h_i, ' ->  ', end='')
+    # print(gen, h_i, z_i_th, ' ->  ', end='')
     # It's impossible to have a double avalanche?
     direction = 1
     while True:
         if site == -1:
             break
-        # print('site \t', site)
         if z_i[site] > z_i_th[site]:
-            # if site == 0 and direction == -1:
-            #     print('!!!')
             site = propagate_avalanche(site)
             direction = -1
         else:
@@ -55,9 +54,19 @@ for gen in range(iterations):
             # break?
         if site == L - 1:
             direction = -1
-    print(h_i)
+    # print(h_i, z_i_th)
+
+    h0.append(h_i[0])
     deviation = np.where(z_i > 2)
     if len(deviation[0]) > 0:
-        print(deviation[0])
-
+        print('ERROR IN THRESHOLDS!', deviation[0])
+# %%
+import matplotlib.pyplot as plt
+# %%
+plt.plot(h0)
+# plt.plot([0,1000],[26.5,26.5])
+np.average(h0[3000:]) # 3000 for L=32; 400 for L = 16
+# %%
+# %%
+plt.bar(np.arange(1, 1 + L), h_i)
 # %%
